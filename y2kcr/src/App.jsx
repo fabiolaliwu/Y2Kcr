6fc7fc94e8fa92b640fc99d59ee5f3f87737bb4b
Fabiola Li Wu <fabiolaliwu@users.noreply.github.com>
2025-07-28 22:12:42 -0400

    updated the contact us

--- y2kcr/src/App.jsx
+++ y2kcr/src/App.jsx
@@ -1,5 +1,8 @@
 import { useState } from 'react'
 import y2kLogo from '/images/logo.png'
+import insta from '/images/instagram.jpg'
+import face from '/images/facebook.jpg'
+import whats from '/images/whatsapp.jpg'
 import './App.css'
 
 function App() {
@@ -14,7 +17,25 @@ function App() {
         aqui posteamos las cosas que vamos a vender 
         </div>
         <div className='contact'>
-        Informacion de contacto
+          <div className='contactUs'>Contact Us</div>
+          <div className='belowContactUs'>
+            <div className='socialMedia'>
+              <div className='box'>
+                <img src={insta} className='smLogos'></img>
+                @_y2kcr
+              </div>
+              <div className='box'>
+                <img src={face} className='smLogos'></img>
+                @_y2kcr
+              </div>
+              <div className='box'>
+                <img src={whats} className='smLogos'></img>
+                +506 8888 8888
+              </div>
+            </div>
+            <div className= 'idk'> </div>
+          </div>
+       
         </div>
       </div>
   )
