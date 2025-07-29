4153c94839f53279502797d2b45b37541a482a81
Fabiola Li Wu <fabiolaliwu@users.noreply.github.com>
2025-07-29 00:25:57 -0400

    reordered the pages

--- y2kcr/src/App.jsx
+++ y2kcr/src/App.jsx
@@ -1,67 +1,11 @@
-import { useState } from 'react'
-import y2kLogo from '/images/logo.png'
-import insta from '/images/instagram.jpg'
-import face from '/images/facebook.jpg'
-import whats from '/images/whatsapp.jpg'
-import './App.css'
+import Landing from './landingPage/landingPage';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-      <div className='mainPage'>
-        <div className='title'>
-          <img src={y2kLogo} className="logo" alt="Y2K Logo" />
-        </div>
-        <div className='body'>
-          EN CONSTRUCCION, YA LLEGAREMOS PRONTO...
-        </div>
-        <div className='contact'>
-          <div className='contactUs'>Contact Us</div>
-          <div className='belowContactUs'>
-            <div className='socialMedia'>
-              <div className='box'>
-                <div className='smLogo'>
-                  <a href='https://www.instagram.com/_y2kcr/' target='_blank' rel='noopener noreferrer'>
-                    <img src={insta} className='smLogos'></img>
-                  </a>
-                </div>
-                <div className='smContact'>
-                  @_y2kcr
-                </div>
-              </div>
-
-              <div className='box'>
-                <div className='smLogo'>
-                  <a href='https://www.facebook.com/_y2kcr/' target='_blank' rel='noopener noreferrer'>
-                    <img src={face} className='smLogos'></img>
-                  </a>
-                </div>
-                <div className='smContact'>
-                  @_y2kcr
-                </div>
-              </div>
-
-              <div className='box'>
-                <div className='smLogo'>
-                  <a href='https://www.whatsapp.com/_y2kcr/' target='_blank' rel='noopener noreferrer'>
-                    <img src={whats} className='smLogos'></img>
-                  </a>
-                </div>
-                <div className='smContact'>
-                  +506 8888 8888
-                </div>
-              </div>
-            </div>
-            <div className= 'us'> 
-              📍 Based in San José, Costa Rica
-              <div className='copyRights'>Copyright ©2025 Y2KCR</div>
-            </div>
-          </div>
-       
-        </div>
-      </div>
-  )
+    <div>
+      <Landing />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ y2kcr/src/landingPage/landingPage.jsx
@@ -0,0 +1,64 @@
+import y2kLogo from '/images/logo.png'
+import insta from '/images/instagram.jpg'
+import face from '/images/facebook.jpg'
+import whats from '/images/whatsapp.jpg'
+import './landingPage.css'
+
+function Landing() {
+  return (
+      <div className='mainPage'>
+        <div className='title'>
+          <img src={y2kLogo} className="logo" alt="Y2K Logo" />
+        </div>
+        <div className='body'>
+          EN CONSTRUCCION, YA LLEGAREMOS PRONTO...
+        </div>
+        <div className='contact'>
+          <div className='contactUs'>Contact Us</div>
+          <div className='belowContactUs'>
+            <div className='socialMedia'>
+              <div className='box'>
+                <div className='smLogo'>
+                  <a href='https://www.instagram.com/_y2kcr/' target='_blank' rel='noopener noreferrer'>
+                    <img src={insta} className='smLogos'></img>
+                  </a>
+                </div>
+                <div className='smContact'>
+                  @_y2kcr
+                </div>
+              </div>
+
+              <div className='box'>
+                <div className='smLogo'>
+                  <a href='https://www.facebook.com/_y2kcr/' target='_blank' rel='noopener noreferrer'>
+                    <img src={face} className='smLogos'></img>
+                  </a>
+                </div>
+                <div className='smContact'>
+                  @_y2kcr
+                </div>
+              </div>
+
+              <div className='box'>
+                <div className='smLogo'>
+                  <a href='https://www.whatsapp.com/_y2kcr/' target='_blank' rel='noopener noreferrer'>
+                    <img src={whats} className='smLogos'></img>
+                  </a>
+                </div>
+                <div className='smContact'>
+                  +506 8888 8888
+                </div>
+              </div>
+            </div>
+            <div className= 'us'> 
+              📍 Based in San José, Costa Rica
+              <div className='copyRights'>Copyright ©2025 Y2KCR</div>
+            </div>
+          </div>
+       
+        </div>
+      </div>
+  )
+}
+
+export default Landing
